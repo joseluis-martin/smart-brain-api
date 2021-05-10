bffbeea6372e32c93c83b473b1970deae5b6e755
joseluis-martin <joseluis.martin@uah.es>
2021-05-10 14:57:16 +0200

    actualizando la base de datos 2

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-perpendicular-61760',
-      user : 'joseluismartinsanchez',
-      password : '',
-      database : 'smart-brain'
+      host : process.env.DATABASE_URL,
+      ssl: true,
     }
   });
 
