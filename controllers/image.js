4404187f993d19d053a579a4939f19fe2f5f1a00
joseluis-martin <joseluis.martin@uah.es>
2021-05-10 19:06:07 +0200

    env variable clarifai key 2

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai') ;
 
 const app = new Clarifai.App({
-    apiKey: process.env.CLARIFAI_API
+    apiKey: process.env.API_CLARIFAI
   });
 
 const handleApiCall = (req, res) => {
