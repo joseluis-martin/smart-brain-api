99be84b53d2f08dc16e5a779c47ba91ac688ae2a
joseluis-martin <joseluis.martin@uah.es>
2021-05-10 18:23:03 +0200

    rehaciendo

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const app =  express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send('it is working!') })
+app.get('/', (req, res) => { res.send('it is working') })
 
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 
